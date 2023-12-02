40bfb906da7fb50ee25371df8610416d3ae5fe28
shubham369963 <shubhamhalade369@gmail.com>
2023-12-02 09:57:39 +0530

    added home, about, contact, added tailwind shubham

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar/>
+      <h1>Hello Rit GDSC</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum officia ipsam numquam amet hic quaerat, dolore sequi quasi ducimus officiis.</p>
+    </div>
+  )
+}
+
+export default HomePage
