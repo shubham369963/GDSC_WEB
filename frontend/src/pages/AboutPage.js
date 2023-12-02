d44e49714b75d8c43fc80bad3ea7700acfc4ab1c
Varad Patil <varadapatil123@gmail.com>
2023-12-02 19:53:04 +0530

    Added: Routing, AboutPage, Created: AboutHeader, Added: color classes in tailwind.config.js,created folder for assests called assets @DevVaradPatil

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutHeader from '../components/AboutHeader'
+
+const AboutPage = () => {
+  return (
+    <div className='w-full flex flex-col px-20'>
+      <AboutHeader />
+    </div>
+  )
+}
+
+export default AboutPage
