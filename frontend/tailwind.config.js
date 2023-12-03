61bb97c31f83e9377afb435b4ab02d75e01ea9f3
Varad Patil <varadapatil123@gmail.com>
2023-12-03 11:40:50 +0530

    About page ui change by @DevVaradPatil

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         "primary-grey": "#656C73"
       },
       boxShadow: {
+        "28": "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
         "big": "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"
       },
       screens: {
